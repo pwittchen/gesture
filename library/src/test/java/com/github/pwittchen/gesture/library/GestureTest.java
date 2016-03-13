e17cd73d02bde3524e6ebd18fb1d55a3aa9b5487
Piotr Wittchen <piotr@wittchen.biz.pl>
2016-03-13 19:27:36 +0100

    updating tests

--- library/src/test/java/com/github/pwittchen/gesture/library/GestureTest.java
+++ library/src/test/java/com/github/pwittchen/gesture/library/GestureTest.java
@@ -1,14 +1,52 @@
 package com.github.pwittchen.gesture.library;
 
+import android.view.MotionEvent;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class GestureTest {
 
   private Gesture gesture;
+  private GestureEvent gestureEvent;
+  @Mock private MotionEvent motionEventDown;
+  @Mock private MotionEvent motionEventMove;
+  @Mock private MotionEvent motionEventUp;
 
   @Before public void setUp() {
     this.gesture = new Gesture();
+    this.gesture.addListener(new GestureListener() {
+      @Override public void onPress(MotionEvent motionEvent) {
+        gestureEvent = GestureEvent.ON_PRESS;
+      }
+
+      @Override public void onTap(MotionEvent motionEvent) {
+        gestureEvent = GestureEvent.ON_TAP;
+      }
+
+      @Override public void onDrag(MotionEvent motionEvent) {
+        gestureEvent = GestureEvent.ON_DRAG;
+      }
+
+      @Override public void onMove(MotionEvent motionEvent) {
+        gestureEvent = GestureEvent.ON_MOVE;
+      }
+
+      @Override public void onRelease(MotionEvent motionEvent) {
+        gestureEvent = GestureEvent.ON_RELEASE;
+      }
+
+      @Override public void onLongPress(MotionEvent motionEvent) {
+        gestureEvent = GestureEvent.ON_LONG_PRESS;
+      }
+
+      @Override public void onMultiTap(MotionEvent motionEvent, int clicks) {
+        gestureEvent = GestureEvent.ON_MULTI_TAP.withClicks(clicks);
+      }
+    });
   }
 
   @Test(expected = IllegalArgumentException.class)
@@ -20,4 +58,6 @@ public class GestureTest {
   public void shouldThrowAnExceptionWhenMotionEventIsNull() {
     gesture.dispatchTouchEvent(null);
   }
+
+  //TODO: implement tests for different methods of the listener
 }
